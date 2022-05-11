258b11be343f78701383c8167eaa57dc9ac41782
danimart <danimart@student.42madrid.com>
2022-05-11 18:26:00 +0200

    client -> server: send PID

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:54:44 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/11 17:33:46 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/11 18:25:39 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,7 @@
 
 int	main(void)
 {
-	/*sigaction	actinf;
-
-	if (sigaction(SIGUSR1, NULL, &actinf) != -1)*/
-
-	kill(13268, SIGUSR1);
+	ft_printf("Client started with PID: %d.\n", getpid());
+	kill(17938, SIGUSR1);
 	return (0);
 }
--- server.c
+++ server.c
@@ -6,26 +6,47 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:52:59 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/11 17:33:09 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/11 18:24:17 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
 
-void	test_handler(int signum)
+void	test_handler(int signum, siginfo_t *info, void *context)
 {
+	context = NULL;
 	if (signum == SIGUSR1)
-		ft_printf("Received SIGUSR1!\n");
+		ft_printf("Received SIGUSR1 from client %d\n", info->si_pid);
 }
 
 int	main(void)
 {
-	int	pid;
+	struct sigaction	sa;
 
-	pid = getpid();
-	ft_printf("Server started with PID: %d.\n", pid);
-	signal(SIGUSR1, test_handler);
+	sa.sa_flags = SA_SIGINFO;
+	sa.sa_sigaction = test_handler;
+	sigaction(SIGUSR1, &sa, NULL);
+	ft_printf("Server started with PID: %d.\n", getpid());
 	while (1)
 		;
 	return (0);
 }
+
+/*
+void    get_pid(int sig, siginfo_t *info, void *context)
+{
+    signalPid = info->si_pid;
+}
+
+int main(int argc, char **argv)
+{
+    struct sigaction        sa;
+
+    printf("PID: %d\n", getpid());       //display PID for kill()
+    sa.sa_flags = SA_SIGINFO;
+    sa.sa_sigaction = get_pid;
+    sigaction(SIGUSR1, &sa, NULL);
+    pause();                           //wait for a signal
+    printf("PID of signal sender = %d\n", signalPid);
+}
+*/
