cdcbf5655f11c00f34956390edfe185435a9192a
danimart <danimart@student.42madrid.com>
2022-05-14 16:30:11 +0200

    Fixed invalid PID handling

--- client.c
+++ client.c
@@ -6,7 +6,7 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:54:44 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/14 16:18:40 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/14 16:27:35 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,46 @@ int	client_stop(int ext_code)
 	exit(ext_code);
 }
 
+int	ft_atoi(const char *str)
+{
+	unsigned long		i;
+	unsigned long long	res;
+	int					sign;
+
+	i = 0;
+	sign = 0;
+	res = 0;
+	while ((str[i] == '\t' || str[i] == '\n' || str[i] == '\v'
+			|| str[i] == '\f' || str[i] == '\r' || str[i] == ' '))
+		i++;
+	if (str[i] == '-' || str[i] == '+')
+		if (str[i++] == '-')
+			sign = 1;
+	while (str[i] >= '0' && str[i] <= '9')
+		res = (res * 10) + (str[i++] - '0');
+	if (res > 9223372036854775807)
+	{
+		if (sign)
+			return (0);
+		return (-1);
+	}
+	if (sign)
+		return (-res);
+	return (res);
+}
+
+int	check_input(int argc, char **args)
+{
+	int	pid;
+
+	if (argc != 3)
+		client_stop(IN_ERR);
+	pid = ft_atoi(args[1]);
+	if (pid <= 0)
+		client_stop(PID_ERR);
+	return (pid);
+}
+
 // ./client [int:PID] [char*:message]
 int	main(int argc, char **args)
 {
@@ -31,11 +71,7 @@ int	main(int argc, char **args)
 
 	i = 0;
 	j = 0;
-	if (argc != 3)
-		return (client_stop(IN_ERR));
-	pid = ft_atoi(args[1]);
-	if (pid == -1)
-		return (client_stop(PID_ERR));
+	pid = check_input(argc, args);
 	while (args[2][i] != '\0')
 	{
 		ch = args[2][i];
