84ced8f3904634fa8607b6fb4670e092ea55368e
danimart <danimart@student.42madrid.com>
2022-05-16 15:14:02 +0200

    Simultaneous client receiving test

--- server.c
+++ server.c
@@ -6,30 +6,73 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:52:59 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/15 20:13:08 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/16 15:10:21 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "minitalk.h"
+#include "server.h"
+
+t_client	get_client(int pid)
+{
+	static int		last_client_id;
+	static t_client	clients[MAX_CONNECTIONS];
+	t_client		client;
+	int				i;
+
+	i = 0;
+	while (i <= last_client_id)
+	{
+		if (clients[i].pid == pid)
+		{
+			ft_printf("Client found at id %d! Returning it.\n", i);
+			return (clients[i]);
+		}
+		ft_printf("Client pid doesn't match at id %d\n", i);
+		i++;
+	}
+	ft_printf("No client matched %d, creating a new t_client for it at id %d...\n", pid, last_client_id);
+	i = 0;
+	client.pid = pid;
+	while (i <= 7)
+		client.bits[i++] = 2;
+	client.ended = 0;
+	clients[last_client_id] = client;
+	last_client_id++;
+	return (client);
+}
+
+static char	store_bit(t_client client, int bit)
+{
+	int	i;
+
+	i = 0;
+	ft_printf("\nStoring received bit for client %d:\n", client.pid);
+	while (client.bits[i] != 2 && i < 8) {
+		ft_printf("i = %d\n", i);
+		i++;
+	}
+	if (i == 7) {
+		ft_printf("Bit pool is full! Returning A.\n");
+		return ('A');
+	} else {
+		ft_printf("Bit pool is not full, adding %d.\n", bit);
+		client.bits[i] = bit;
+	}
+	return ('\0');
+}
 
 static void	signal_handler(int signum, siginfo_t *info, void *context)
 {
-	static int	bit_count = 0;
-	static char	ch = 0;
+	char			ch;
 
-	(void) info;
+	ft_printf("\n---------------------------\nSIGNAL RECEIVED WITH NUM %d\n---------------------------\n", signum);
 	(void) context;
 	if (signum == SIGUSR1)
-		ch = ch | 128 >> bit_count;
-	bit_count++;
-	if (bit_count == 8)
-	{
-		if (ch == '\0')
-			ft_printf(":END\n");
+		ch = store_bit(get_client(info->si_pid), 1);
+	else
+		ch = store_bit(get_client(info->si_pid), 0);
+	if (ch != '\0')
 		ft_printf("%c", ch);
-		bit_count = 0;
-		ch = 0;
-	}
 }
 
 int	main(void)
--- server.h
+++ server.h
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.h                                           :+:      :+:    :+:   */
+/*   server.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/11 20:16:49 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/15 21:44:13 by danimart         ###   ########.fr       */
+/*   Created: 2022/05/15 21:45:19 by danimart          #+#    #+#             */
+/*   Updated: 2022/05/15 22:12:18 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef CLIENT_H
-# define CLIENT_H
+#ifndef SERVER_H
+# define SERVER_H
 
 # include "minitalk.h"
 
-# define IN_ERR 1
-# define IN_ERR_STR "\e[0;31mError\e[1;30m: \e[0;31mInvalid input\e[1;30m.\e[0m\n"
-# define PID_ERR 2
-# define PID_ERR_STR "\e[0;31mError\e[1;30m: \e[0;31mInvalid PID\e[1;30m.\e[0m\n"
-# define SIG_SEND_ERR 3
-# define SIG_SEND_ERR_STR "\e[0;31mError\e[1;30m: \e[0;31mFailed to send signal,\
- is the PID correct?\e[0m\n"
+# define MAX_CONNECTIONS 1024
 
-int	ft_atoi(const char *pid_str);
+typedef struct s_client {
+	int	pid;
+	int	bits[8];
+	int	ended;
+}				t_client;
 
 #endif
