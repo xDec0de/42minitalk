2587d01c709488ee79868f428bf4679bb1ee3ceb
danimart <danimart@student.42madrid.com>
2022-05-16 21:16:29 +0200

    Cleaned unused code

--- server.c
+++ server.c
@@ -6,19 +6,17 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:52:59 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/16 21:07:23 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/16 21:15:51 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-static void	signal_handler(int signum, siginfo_t *info, void *context)
+static void	signal_handler(int signum)
 {
 	static int	bit_count = 0;
 	static char	ch = 0;
 
-	(void) info;
-	(void) context;
 	if (signum == SIGUSR1)
 		ch = ch | 128 >> bit_count;
 	bit_count++;
@@ -32,12 +30,8 @@ static void	signal_handler(int signum, siginfo_t *info, void *context)
 
 int	main(void)
 {
-	struct sigaction	sa;
-
-	sa.sa_flags = SA_SIGINFO;
-	sa.sa_sigaction = signal_handler;
-	sigaction(SIGUSR1, &sa, NULL);
-	sigaction(SIGUSR2, &sa, NULL);
+	signal(SIGUSR1, signal_handler);
+	signal(SIGUSR2, signal_handler);
 	ft_printf(PID_NOTIFY, getpid());
 	while (1)
 		;
