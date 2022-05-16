8fbfde25b5f45d7328badab6e0f64cfcffca8eb5
danimart <danimart@student.42madrid.com>
2022-05-16 20:37:10 +0200

    Multi-client support added (Useless because signals break anyways :D)

--- server.c
+++ server.c
@@ -6,21 +6,26 @@
 /*   By: danimart <danimart@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 12:52:59 by danimart          #+#    #+#             */
-/*   Updated: 2022/05/16 19:13:21 by danimart         ###   ########.fr       */
+/*   Updated: 2022/05/16 20:30:44 by danimart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "server.h"
 
-void	print_bit_array(int arr[])
+char	bit_array_to_char(int arr[])
 {
-	int	i;
+	int		i;
+	char	ch;
 
+	ch = 0;
 	i = 0;
-	ft_printf("Bit array = (");
 	while (i < 8)
-		ft_printf("%d", arr[i++]);
-	ft_printf(")\n");
+	{
+		if (arr[i] == 1)
+			ch = ch | 128 >> i;
+		i++;
+	}
+	return (ch);
 }
 
 t_client	*get_client(int pid)
@@ -34,11 +39,7 @@ t_client	*get_client(int pid)
 	while (i < last_client_id)
 	{
 		if (clients[i].pid == pid)
-		{
-			ft_printf("Client found at id %d! Returning it.\n\n", i);
 			return (&clients[i]);
-		}
-		ft_printf("Client pid doesn't match %d at id %d\n", clients[i].pid, i);
 		i++;
 	}
 	i = 0;
@@ -53,7 +54,8 @@ t_client	*get_client(int pid)
 
 static char	store_bit(t_client *client, int bit)
 {
-	int	i;
+	int		i;
+	char	ch;
 
 	i = 0;
 	while (i < 8)
@@ -62,18 +64,17 @@ static char	store_bit(t_client *client, int bit)
 			break ;
 		i++;
 	}
-	if (i == 8) {
-		ft_printf("Bit array is full! Clearing and returning A.\n");
-		print_bit_array(client->bits);
+	if (i == 8)
+	{
 		i = 0;
+		ch = bit_array_to_char(client->bits);
 		while (i <= 7)
 			client->bits[i++] = 2;
-		return ('A');
-	} else {
-		ft_printf("Bit array is not full, adding %d.\n", bit);
-		print_bit_array(client->bits);
-		client->bits[i] = bit;
+		client->bits[0] = bit;
+		return (ch);
 	}
+	else
+		client->bits[i] = bit;
 	return ('\0');
 }
 
@@ -83,7 +84,6 @@ static void	signal_handler(int signum, siginfo_t *info, void *context)
 	int				pid;
 	t_client		*client;
 
-	ft_printf("\n---------------------------\nSIGNAL RECEIVED WITH NUM %d\n---------------------------\n", signum);
 	(void) context;
 	pid = info->si_pid;
 	client = get_client(pid);
