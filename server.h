51cd0655137a9c2c7bfb4264d37fa59f76a963bb
xDec0de <xdec0deyt@gmail.com>
2025-02-10 17:48:32 +0100

    Remove MAX_CONNECTIONS (Unused)

--- server.h
+++ server.h
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/15 21:45:19 by daniema3          #+#    #+#             */
-/*   Updated: 2025/02/10 16:29:35 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/02/10 17:47:01 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,6 @@
 # define SERVER_H
 
 # include "minitalk.h"
-
-# define MAX_CONNECTIONS 1024
 
 # define PID_NOTIFY "\e[0;36mServer started with PID\e[1;30m:\
  \e[1;33m%d\e[0m\n"
