0fa60c0eb236b223fd55b2af401fa6fdcc8ea087
xDec0de <xdec0deyt@gmail.com>
2025-02-18 17:52:10 +0100

    Added t_ulong

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 13:04:32 by daniema3          #+#    #+#             */
-/*   Updated: 2025/02/14 15:42:06 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/02/18 17:41:52 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,12 @@
 
 # include "printf/ft_printf.h"
 # include <signal.h>
+# include <stdio.h>
 
 # ifndef SIGNAL_SLEEP
 #  define SIGNAL_SLEEP 250
 # endif
+
+typedef unsigned long long	t_ulong;
 
 #endif
