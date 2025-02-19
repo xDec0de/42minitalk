3792350ab4f95b436fecec8232e430e192e9ddab
xDec0de <xdec0deyt@gmail.com>
2025-02-19 16:11:48 +0100

    Added ULONG_BIT macro

--- minitalk.h
+++ minitalk.h
@@ -6,7 +6,7 @@
 /*   By: daniema3 <daniema3@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 13:04:32 by daniema3          #+#    #+#             */
-/*   Updated: 2025/02/18 17:52:31 by daniema3         ###   ########.fr       */
+/*   Updated: 2025/02/19 16:09:06 by daniema3         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,13 @@
 
 # include "printf/ft_printf.h"
 # include <signal.h>
+# include <limits.h>
 
 # ifndef SIGNAL_SLEEP
 #  define SIGNAL_SLEEP 250
 # endif
 
 typedef unsigned long long	t_ulong;
+# define ULONG_BIT CHAR_BIT * sizeof(t_ulong)
 
 #endif
